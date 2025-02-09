77cf72756a7ee80087edbd2307576496fb7261f4
Jeon Hyejin <hyejda00@naver.com>
2025-02-09 17:27:16 +0900

    Setting: 프로젝트 초기 세팅

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      "no-unused-vars":"off", // 코드상에 사용하지 않는 변수가 있을 때 오류로 알려주는 옵션
+      "react/prop-types":"off", // 리액트를 조금 더 안전하게 사용하게 해주는 옵션
+    },
+  },
+)
