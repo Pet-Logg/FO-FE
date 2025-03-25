d783f2ada9265251b0614b15f0038c69e9b17cb4
Jeon Hyejin <hyejda00@naver.com>
2025-03-25 15:24:59 +0900

    Feat: 리액트쿼리 devtools 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { queryClient } from './utils/queryClient.ts'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 createRoot(document.getElementById('root')!).render(
   <div className='flex flex-col'>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition='bottom-left' />
       <App />
     </QueryClientProvider>
   </div>
