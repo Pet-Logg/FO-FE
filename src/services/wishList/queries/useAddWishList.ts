3ea9a43e89773a320c651851a25abe4646b58bbc
Jeon Hyejin <hyejda00@naver.com>
2025-03-26 17:48:34 +0900

    Fix: 장바구니 담기 두번이상 누를 경우 장바구니에 상품 계속 추가되는 오류 해결

--- src/services/wishList/queries/useAddWishList.ts
+++ src/services/wishList/queries/useAddWishList.ts
@@ -1,10 +1,17 @@
 import { UseMutationCustomOptions } from '@/types/api'
 import { useMutation } from '@tanstack/react-query'
 import { addWishList } from '../api'
+import { queryClient } from '@/utils/queryClient'
+import { queryKeys } from '@/constants'
 
 export const useAddWishList = (mutationOptions?: UseMutationCustomOptions) => {
   return useMutation({
     mutationFn: addWishList,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [queryKeys.WISHLIST, queryKeys.GET_WISHLIST]
+      })
+    },
     throwOnError: (error) => Number(error.response?.status) >= 500,
     ...mutationOptions
   })
