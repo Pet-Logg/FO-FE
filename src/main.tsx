e5cf363c7b958ca24c167003a65dac421fee3116
Jeon Hyejin <hyejda00@naver.com>
2025-04-10 01:45:41 +0900

    fix: 빌드 시 import문 오류 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
-import { queryClient } from './utils/queryClient.ts'
+import { queryClient } from './utils/queryClient'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 createRoot(document.getElementById('root')!).render(
