60e84ed07a923ce5e2800a3ddc560b42d24599e3
Jeon Hyejin <hyejda00@naver.com>
2025-04-29 09:00:45 +0900

    Fix: deploy 수정

--- src/services/order/queries/useCreateOrder.ts
+++ src/services/order/queries/useCreateOrder.ts
@@ -1,14 +1,10 @@
 import { UseMutationCustomOptions } from '@/types/api'
-import { queryClient } from '@/utils/queryClient'
 import { useMutation } from '@tanstack/react-query'
 import { createOrder } from '../api'
 
 export const useCreateOrder = (mutationOptions?: UseMutationCustomOptions) => {
   return useMutation({
     mutationFn: createOrder,
-    onSuccess: () => {
-      queryClient.invalidateQueries({})
-    },
     throwOnError: (error) => Number(error.response?.status) >= 500,
     ...mutationOptions
   })
